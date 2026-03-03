654f7188943bcc676d1e094497369a130e2c0d25
zhq666666 <aaa.fei24@gmail.com>
2026-03-03 17:33:17 +0800

    Updated package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [tailwind()],
 });
